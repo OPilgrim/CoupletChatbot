93862a6750ddaf8b686184c1d69b8351b832584c
Raining Ow <rainingow@163.com>
2020-11-11 23:35:21 +0800

    Update settings.py

--- CoupletChatbot/settings.py
+++ CoupletChatbot/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coupletchatbot',
     'django_crontab',
     'couplet_dialog',
-    'coupletchatbot',
+
 ]
 
 MIDDLEWARE = [
